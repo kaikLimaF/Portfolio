9b3e0c5b2b78fd9aed0107c9cd3cd794fdc82a92
José kaik Lima Ferreira <133561951+kaikLimaF@users.noreply.github.com>
2025-09-16 11:26:04 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+ document.querySelectorAll("[data-scroll]").forEach(botao => {
+    botao.addEventListener("click", () => {
+      const destino = document.querySelector(botao.dataset.scroll);
+      if(destino) destino.scrollIntoView({ behavior: "smooth" });
+    });
+  });
+
+  document.querySelectorAll('a[href^="#"]').forEach(link => {
+  link.addEventListener("click", function(e) {
+    const destino = document.querySelector(this.getAttribute("href"));
+    if(destino) {
+      e.preventDefault(); // impede comportamento padrão
+      destino.scrollIntoView({ behavior: "smooth" });
+    }
+  });
+});
+
+document.getElementById("botao-cv").addEventListener("click", () => {
+  window.open("/img/CurriculoJoseKaik.pdf", "_blank");
+});
